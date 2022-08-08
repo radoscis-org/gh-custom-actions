663b9bf0490eb9b276c681a1960c65076d2353ac
Radoslaw Cisz <radoslaw.cisz@gmail.com>
2022-08-08 15:45:07 +0200

    fix set-env command

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -24,4 +24,13 @@ echo "Some Stuff"
 echo "::endgroup::"
 
 # Export env variable
-echo '::set-env name=HELLO::hello from docker'
+# https://github.blog/changelog/2020-10-01-github-actions-deprecating-set-env-and-add-path-commands/
+# echo '::set-env name=HELLO::hello from docker'
+
+#New way to set env variables
+# https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-environment-variable
+
+echo "HELLO=hello form docker" >> $GITHUB_ENV
+# Set Fail in ourt action
+# In sh script just exit with non-zero error code
+# exit 1
